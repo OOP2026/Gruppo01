4047491ff7b386b00284e91be93611433f4c678c
Fredd44 <feder.merola@studenti.unina.it>
2026-05-22 11:09:04 +0200

    primi passi gui

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,8 +1,20 @@
 package gui;
+import controller.Controller;
+import javax.swing.*;
+import java.awt.*;
 
-public class Home {
+public class Home extends JFrame {
+	private Controller controller;
+
+	public Home() {
+		controller= new Controller();
+	}
+	private JPanel finestra1;
+
 	public static void main(String[] args) {
+
+
 
 	}
 
-}
+
