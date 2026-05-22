9bd00d92888c70a6ffbd1471bd92076fc3441365
kage-012 <pasqualemencocco3@gmail.com>
2026-05-22 11:37:44 +0200

    aggiornamento gui

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -7,14 +7,18 @@ public class Home extends JFrame {
 	private Controller controller;
 
 	public Home() {
-		controller= new Controller();
+		this.controller= new Controller();
 	}
 	private JPanel finestra1;
+	private Window Finestra;
 
 	public static void main(String[] args) {
-
-
-
+		Home interfaccia = new Home();
+		JFrame Frame = new JFrame("Home");
+		Frame.setContentPane(new Home().finestra1);
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.pack();
+		Frame.setVisible(true);
 	}
-
+}
 
